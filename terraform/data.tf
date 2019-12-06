96b005217540386473947cc09a73374308bc500f
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-06 15:34:18 +0000

    [PRMT-720] Fix inbound service connection and credentials

--- terraform/data.tf
+++ terraform/data.tf
@@ -11,3 +11,19 @@ data "aws_ssm_parameter" "spine_org_code" {
 data "aws_vpc" "supplier_vpc" {
   id = var.supplier_vpc_id
 }
+
+data "aws_secretsmanager_secret" "mq-app-username" {
+  name = "/nhs/${var.deductions_env}/mq/app-username"
+}
+
+data "aws_secretsmanager_secret" "mq-app-password" {
+  name = "/nhs/${var.deductions_env}/mq/app-password"
+}
+
+data "aws_ssm_parameter" "amqp-endpoint-0" {
+  name        = "/NHS/${var.deductions_env}-${data.aws_caller_identity.current.account_id}/amqp-endpoint/0"
+}
+
+data "aws_ssm_parameter" "amqp-endpoint-1" {
+  name        = "/NHS/${var.deductions_env}-${data.aws_caller_identity.current.account_id}/amqp-endpoint/1"
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,9 +3,8 @@ locals {
   mhs_spine_org_code   = data.aws_ssm_parameter.spine_org_code.value
   ecr_address = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com" # created in prm-deductions-base-infra
 
-  #FIXME: read these from SSM
-  inbound_queue_host="amqp+ssl://b-93946876-8604-4815-8ef3-eb4f1dc68c95-1.mq.eu-west-2.amazonaws.com:5671"
-  inbound_queue_username_arn="arn:aws:secretsmanager:eu-west-2:${local.account_id}:secret:/nhs/dev/mq2/username-c29IDe"
-  inbound_queue_password_arn="arn:aws:secretsmanager:eu-west-2:${local.account_id}:secret:/nhs/dev/mq2/password-VptMfq"
-
+  inbound_queue_username_arn=data.aws_secretsmanager_secret.mq-app-username.arn
+  inbound_queue_password_arn=data.aws_secretsmanager_secret.mq-app-password.arn
+  #FIXME should use a failover connection string with both endpoints
+  inbound_queue_host=replace(data.aws_ssm_parameter.amqp-endpoint-1.value, "amqp+ssl", "amqps")
 }
