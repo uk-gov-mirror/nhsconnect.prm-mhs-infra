cda9ee5c3fd74314c224f828392b7fbbe257f91f
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-13 16:38:59 +0000

    [PRMT-774] Migrate terraform backend to allow multiple envs

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,6 @@
 terraform{
       backend "s3" {
         bucket  = "prm-deductions-terraform-state"
-        key     = "mhs/terraform.tfstate"
         region  = "eu-west-2"
         encrypt = true
     }
