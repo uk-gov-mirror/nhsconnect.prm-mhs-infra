fa25c9acd0494f77989cfa36e15c0afae102ba01
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-27 17:06:58 +0000

    Export MHS URLs to SSM

--- terraform/output.tf
+++ terraform/output.tf
@@ -17,19 +17,19 @@ variable "deductions_env" {
 resource "aws_ssm_parameter" "outbound_url" {
   name = "/NHS/deductions-${data.aws_caller_identity.current.account_id}/mhs-${var.environment_id}/outbound_url"
   type  = "String"
-  value = trimsuffix("https://mhs-outbound.${aws_route53_zone.mhs_hosted_zone.name}", ".")
+  value = trimsuffix("https://${aws_route53_record.mhs_outbound_load_balancer_record.name}", ".")
 }
 
 resource "aws_ssm_parameter" "inbound_url" {
   name = "/NHS/deductions-${data.aws_caller_identity.current.account_id}/mhs-${var.environment_id}/inbound_url"
   type  = "String"
-  value = trimsuffix("https://mhs-inbound.${aws_route53_zone.mhs_hosted_zone.name}", ".")
+  value = trimsuffix("${aws_route53_record.mhs_inbound_load_balancer_record.name}", ".")
 }
 
 resource "aws_ssm_parameter" "route_url" {
   name = "/NHS/deductions-${data.aws_caller_identity.current.account_id}/mhs-${var.environment_id}/route_url"
   type  = "String"
-  value = trimsuffix("https://mhs-route.${aws_route53_zone.mhs_hosted_zone.name}", ".")
+  value = trimsuffix("https://${aws_route53_record.mhs_route_load_balancer_record.name}", ".")
 }
 
 resource "aws_ssm_parameter" "mhs_vpc" {
