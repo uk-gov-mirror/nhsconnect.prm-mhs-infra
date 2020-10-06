73875e87d7d97edbb512548d22512373916a4fd7
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-10-06 17:56:48 +0100

    [PRMT-1093] Added default gateway in public subnet in mhs ptl

--- terraform/route.tf
+++ terraform/route.tf
@@ -13,3 +13,18 @@ resource "aws_route_table_association" "public" {
   subnet_id      = local.public_subnet_id
   route_table_id = local.public_subnet_route_table
 }
+
+resource "aws_route" "internet" {
+  count = var.use_existing_vpc == "" ? 0 : 1
+  route_table_id = local.public_subnet_route_table
+  gateway_id = split("/", join(",", data.aws_internet_gateway.default.*.arn))[1]
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+data "aws_internet_gateway" "default" {
+  count = var.use_existing_vpc == "" ? 0 : 1
+  filter {
+    name   = "attachment.vpc-id"
+    values = [local.mhs_vpc_id]
+  }
+}
