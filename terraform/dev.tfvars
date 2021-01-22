873b89a01c8d292486d05e758d865a4857881dbb
Tomasz Setkowski <tomasz.setkowski2@nhs.net>
2021-01-22 13:34:13 +0000

    [PRMT-1450] Added elasticache deployment for mhs-route

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -5,3 +5,4 @@ mhs_state_table_write_capacity = 5
 repo_name = "prm-mhs-infra"
 mhs_sync_async_table_read_capacity = 5
 mhs_sync_async_table_write_capacity = 5
+elasticache_node_type               = "cache.t3.micro"
---
+++ terraform/elasticache.tf
@@ -0,0 +1,41 @@
+# ElastiCache (Redis) is used by mhs-route to cache SDS queries
+
+resource "aws_elasticache_replication_group" "elasticache_replication_group" {
+  automatic_failover_enabled = true
+  replication_group_id = "${var.environment}-${var.cluster_name}-rg"
+  replication_group_description = "An ElastiCache cluster for ${var.cluster_name} in the environment: ${var.environment}"
+  node_type = var.elasticache_node_type
+  number_cache_clusters = length(local.mhs_private_subnet_ids)
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+  port = 6379
+  subnet_group_name = aws_elasticache_subnet_group.elasticache_subnet_group.name
+  security_group_ids = [
+    aws_security_group.sds_cache_security_group.id
+  ]
+
+  tags = {
+    Environment = var.environment
+    CreatedBy = var.repo_name
+  }
+}
+
+# The MHS ElastiCache subnet group. Defines the subnets that the ElastiCache cluster should place replicas in.
+resource "aws_elasticache_subnet_group" "elasticache_subnet_group" {
+  name = "${var.environment}-${var.cluster_name}-elasticache-subnet-group"
+  description = "Subnet group for the ElastiCache cluster for ${var.cluster_name} in environment: ${var.environment}"
+  subnet_ids = local.mhs_private_subnet_ids
+}
+
+# SDS cache security group
+resource "aws_security_group" "sds_cache_security_group" {
+  name = "${var.environment}-${var.cluster_name}-elasticache-subnet-group"
+  description = "The security group used to control traffic for the SDS cache endpoint."
+  vpc_id = local.mhs_vpc_id
+
+  tags = {
+    Name = "${var.environment}-sds-cache-sg"
+    Environment = var.environment
+    CreatedBy = var.repo_name
+  }
+}
