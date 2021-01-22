b52046439bac2190a3533615641513a5964c7910
Tomasz Setkowski <tomasz.setkowski2@nhs.net>
2021-01-22 13:34:13 +0000

    [PRMT-1450] Added elasticache deployment for mhs-route

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,7 @@ variable "mhs_state_table_read_capacity" {}
 variable "mhs_state_table_write_capacity" {}
 variable "mhs_sync_async_table_read_capacity" {}
 variable "mhs_sync_async_table_write_capacity" {}
+
+variable "elasticache_node_type" {
+  description = "The type of ElastiCache node to use when deploying the ElastiCache cluster. Possible node types can be found from https://aws.amazon.com/elasticache/features/#Available_Cache_Node_Types"
+}
